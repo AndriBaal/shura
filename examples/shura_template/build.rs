8853384f80ad9191ad9549b2222afe75009e4989
Andri <80914617+Shuray04@users.noreply.github.com>
2023-11-02 22:49:11 +0100

    wip

--- examples/shura_template/build.rs
+++ examples/shura_template/build.rs
@@ -7,34 +7,45 @@ fn main() {
     let target_os = env::var("CARGO_CFG_TARGET_OS");
     match target_os.as_ref().map(|x| &**x) {
         Ok("android") => (),
-        Ok(_) => {
+        Ok(os) => {
             println!("cargo:rerun-if-changed=res/*");
 
             let cargo_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
             let root = Path::new(&cargo_dir);
             let res = root.join("res");
             if res.exists() {
-                let build_type = env::var("PROFILE").unwrap();
-                let dest = env::var("CARGO_TARGET_DIR")
+                let mut copy_options = CopyOptions::new();
+                copy_options.overwrite = true;
+                let target_arch = std::env::var("CARGO_CFG_TARGET_ARCH").unwrap();
+                let target_dir = env::var("CARGO_TARGET_DIR")
                     .map(|dir| Path::new(&dir).to_path_buf())
-                    .unwrap_or(root.join("target"))
-                    .join(build_type);
+                    .unwrap_or(root.join("target"));
+                if os == "unknown" && target_arch == "wasm32" {
+                    let dest = target_dir
+                        .join("wasm-examples")
+                        .join("desktop");
 
-                if dest.exists() {
-                    let mut copy_options = CopyOptions::new();
-                    copy_options.overwrite = true;
                     let mut paths_to_copy = Vec::new();
-                    paths_to_copy.push("res/");
-
+                    paths_to_copy.push(res);
                     copy_items(&paths_to_copy, dest.clone(), &copy_options).unwrap();
+                } else {
+                    let build_type = env::var("PROFILE").unwrap();
+                    let dest = target_dir.join(build_type);
 
-                    let examples = dest.join("examples");
-                    if examples.exists() {
-                        copy_items(&paths_to_copy, examples, &copy_options).unwrap();
+                    if dest.exists() {
+                        let mut paths_to_copy = Vec::new();
+                        paths_to_copy.push(res);
+
+                        copy_items(&paths_to_copy, dest.clone(), &copy_options).unwrap();
+
+                        let examples = dest.join("examples");
+                        if examples.exists() {
+                            copy_items(&paths_to_copy, examples, &copy_options).unwrap();
+                        }
                     }
                 }
             }
         }
-        _ => {}
+        _ => (),
     }
 }
