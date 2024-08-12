5bcf7d6b3872bf8bc0cc99db1ea6744d6240ae64
Andri <andri.baal@bluewin.ch>
2024-08-12 09:00:04 +0200

    wip

--- shura_template/src/main.rs
+++ shura_template/src/main.rs
@@ -4,9 +4,7 @@ use shura::prelude::*;
 fn app(config: AppConfig) {
     App::run(config, || {
         Scene::new()
-            .render_group3d("cube", RenderGroupUpdate::EVERY_FRAME)
-            .entity::<Cube>()
-            .entity_single::<Assets>()
+            .entity::<Bunny>()
             .system(System::update(update))
             .system(System::setup(setup))
             .system(System::render(render))
@@ -14,108 +12,144 @@ fn app(config: AppConfig) {
 }
 
 fn setup(ctx: &mut Context) {
-    const NUM_INSTANCES_PER_ROW: u32 = 10;
-    const SPACE_BETWEEN: f32 = 3.0;
-    let cubes = (0..NUM_INSTANCES_PER_ROW)
-        .flat_map(|z| {
-            (0..NUM_INSTANCES_PER_ROW).map(move |x| {
-                let x = SPACE_BETWEEN * (x as f32 - NUM_INSTANCES_PER_ROW as f32 / 2.0);
-                let z = SPACE_BETWEEN * (z as f32 - NUM_INSTANCES_PER_ROW as f32 / 2.0);
-                let position = Vector3::new(x, 0.0, z);
-
-                Cube::new(position)
-            })
-        })
-        .collect::<Vec<_>>();
-    ctx.entities.get_mut().add_many(ctx.world, cubes);
-
-    let gpu = ctx.gpu.clone();
-    let assets = ctx.assets.clone();
-    ctx.tasks
-        .spawn(move || { Assets::new(&*assets, &gpu) }, |ctx, res| {
-            ctx.entities.single_mut().set(ctx.world, res);
-        });
+    ctx.world_camera2d.set_scaling(WorldCameraScaling::Min(3.0));
+    info!("hello world");
+    info!("hello world");
+    ctx.assets.load_font(
+        "font",
+        FontBuilder::bytes(include_asset_bytes!("novem.ttf")),
+    );
+    info!("hello world2");
+    ctx.assets.load_text_mesh::<&str>("text", "font", &[]);
+    info!("hello world3");
+    ctx.assets.load_smart_instance_buffer::<SpriteInstance2D>(
+        "bunny_instances",
+        SmartInstanceBuffer::EVERY_FRAME,
+    );
+    info!("hello world4");
+    ctx.assets.load_sprite(
+        "bunny_sprite",
+        SpriteBuilder::bytes(include_asset_bytes!("wabbit.png")),
+    );
+    info!("hello world5");
+    ctx.entities
+        .get_mut::<Bunny>()
+        .add(ctx.world, Bunny::new(Default::default()));
+    info!("hello world6");
 }
 
 fn update(ctx: &mut Context) {
-    const SPEED: f32 = 7.0;
-    if ctx.entities.single::<Assets>().is_none() {
-        return;
+    info!("hello world1");
+    const MODIFY_STEP: usize = 1500;
+    const GRAVITY: f32 = -2.5;
+
+    let mut bunnies = ctx.entities.get_mut::<Bunny>();
+
+    if ctx.input.is_held(MouseButton::Left) || ctx.input.is_held(ScreenTouch) {
+        let cursor: Vector2<f32> = ctx.cursor.coords;
+        for _ in 0..MODIFY_STEP {
+            bunnies.add(ctx.world, Bunny::new(cursor));
+        }
+    }
+    if ctx.input.is_held(MouseButton::Right) {
+        let mut dead: Vec<EntityHandle> = vec![];
+        for bunny in bunnies.iter().rev() {
+            if dead.len() == MODIFY_STEP {
+                break;
+            }
+            dead.push(bunny.handle);
+        }
+        for handle in dead {
+            bunnies.remove(ctx.world, &handle);
+        }
     }
 
-    let speed = SPEED * ctx.time.delta();
-    let camera = ctx.world_camera3d.perspective_mut().unwrap();
+    ctx.assets.write_text(
+        "text",
+        "font",
+        &[TextSection {
+            color: Color::RED,
+            text: format!("FPS: {}\nBunnies: {}", ctx.time.fps(), bunnies.len()),
+            size: 0.05,
+            horizontal_alignment: TextAlignment::End,
+            vertical_alignment: TextAlignment::End,
+            ..Default::default()
+        }],
+    );
 
-    let forward = camera.target - camera.eye;
-    let forward_norm = forward.normalize();
-    let forward_mag = forward.magnitude();
+    let delta = ctx.time.delta();
+    let fov = ctx.world_camera2d.fov();
 
-    if ctx.input.is_held(Key::KeyW) && forward_mag > speed {
-        camera.eye += forward_norm * speed;
-    }
-    if ctx.input.is_held(Key::KeyS) {
-        camera.eye -= forward_norm * speed;
-    }
+    for bunny in bunnies.iter_mut() {
+        let mut linvel = bunny.linvel;
+        let mut translation = bunny.position.translation.vector;
 
-    let right = forward_norm.cross(&camera.up);
-    let forward = camera.target - camera.eye;
-    let forward_mag = forward.magnitude();
+        linvel.y += GRAVITY * delta;
+        translation += linvel * delta;
+        if translation.x >= fov.x {
+            linvel.x = -linvel.x;
+            translation.x = fov.x;
+        } else if translation.x <= -fov.x {
+            linvel.x = -linvel.x;
+            translation.x = -fov.x;
+        }
 
-    if ctx.input.is_held(Key::KeyD) {
-        camera.eye = camera.target - (forward + right * speed).normalize() * forward_mag;
-    }
-
-    if ctx.input.is_held(Key::KeyA) {
-        camera.eye = camera.target - (forward - right * speed).normalize() * forward_mag;
-    }
-
-    for cube in ctx.entities.get_mut::<Cube>().iter_mut() {
-        let mut rot = cube.position.rotation();
-        rot *= Rotation3::new(Vector3::new(
-            1.0 * ctx.time.delta(),
-            1.0 * ctx.time.delta(),
-            1.0 * ctx.time.delta(),
-        ));
-        cube.position.set_rotation(rot);
+        if translation.y < -fov.y {
+            linvel.y = gen_range(0.0..15.0);
+            translation.y = -fov.y;
+        } else if translation.y > fov.y {
+            linvel.y = -1.0;
+            translation.y = fov.y;
+        }
+        bunny.linvel = linvel;
+        bunny.position.translation.vector = translation;
     }
 }
 
 fn render(ctx: &RenderContext, encoder: &mut RenderEncoder) {
-    if let Some(assets) = ctx.entities.single::<Assets>().get() {
-        encoder.render3d(
-            Some(RgbaColor::new(220, 220, 220, 255).into()),
-            |renderer| {
-                ctx.group("cube", |buffer| {
-                    renderer.draw_model(buffer, ctx.world_camera3d, &assets.model);
-                });
-            },
-        );
-    }
+    encoder.render2d(
+        Some(RgbaColor::new(220, 220, 220, 255).into()),
+        |renderer| {
+            renderer.draw_sprite(
+                &ctx.assets.instances("bunny_instances"),
+                &ctx.default_assets.world_camera2d,
+                &ctx.default_assets.sprite_mesh,
+                &ctx.assets.sprite("bunny_sprite"),
+            );
+
+            renderer.draw_text_mesh(
+                &ctx.assets.text_mesh("text"),
+                &ctx.assets.font("font"),
+                &ctx.default_assets.relative_top_right_camera.0,
+            );
+        },
+    );
 }
 
 #[derive(Entity)]
-struct Assets {
-    model: Model,
+#[shura(
+    asset = "bunny_instances", 
+    ty = SmartInstanceBuffer<SpriteInstance2D>,
+    action = |bunny, asset, _| asset.push(SpriteInstance2D::new(bunny.position, bunny.scaling, ()));
+)]
+struct Bunny {
+    #[shura(component)]
+    handle: EntityHandle,
+    position: Isometry2<f32>,
+    scaling: Vector2<f32>,
+    linvel: Vector2<f32>,
 }
 
-impl Assets {
-    pub fn new(assets: &dyn AssetManager, gpu: &Gpu) -> Self {
-        Self {
-            model: gpu.create_model(assets.load_model("3d/cube/cube.obj")),
-        }
-    }
-}
-
-#[derive(Entity)]
-struct Cube {
-    #[shura(component = "cube")]
-    position: PositionComponent3D,
-}
-
-impl Cube {
-    pub fn new(position: Vector3<f32>) -> Cube {
-        Cube {
-            position: PositionComponent3D::new().with_translation(position), // .with_scaling(Vector3::new(0.001, 0.001, 0.001)),
+impl Bunny {
+    pub fn new(translation: Vector2<f32>) -> Bunny {
+        let scaling = gen_range(0.75_f32..2.0) * vector!(0.12, 0.18);
+        let rotation = gen_range(-1.0..1.0);
+        let linvel = vector!(gen_range(-2.5..2.5), gen_range(-7.5..7.5));
+        Bunny {
+            position: Isometry2::new(translation, rotation),
+            linvel,
+            handle: Default::default(),
+            scaling,
         }
     }
 }
